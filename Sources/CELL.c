090a1d603d0e64ebb3c8a8f4d3469637d4f0cab1
Angelo <Soviet_Trooper@live.ca>
2020-10-29 21:39:01 -0700

    Update CELL.c

--- Sources/CELL.c
+++ Sources/CELL.c
@@ -1,3 +1,17 @@
+/*
+  Author: Angelo Sanches
+  Board:  mc9s12xdp512(nait)
+  Perpous: To Create and maintain a connection with the cellphone model
+  Created: ?  
+  
+  -------------UseLog-------------
+  ->last is template
+  Modified:<version:Date>
+  Reason:
+  ----------Change Log------------
+  ->last is template
+  Log:<Reason/Modified> Optional log Optional log in case of compliations
+*/
 #include "CELL.h"
 //Opens the cellphone models serial port and sets up for use.
 void CELLInIt(void)
