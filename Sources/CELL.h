ffb594f53e58033bea5d76858fddec23a6324050
Angelo <Soviet_Trooper@live.ca>
2020-10-29 21:37:32 -0700

    Update CELL.h
    Commenting

--- Sources/CELL.h
+++ Sources/CELL.h
@@ -7,11 +7,22 @@
 #include "MessageMaker.h"
 #include "DirtyDelays.h"
 
-
+//Opens the com ports and sets up a Serial port for use.
 void CELLInIt(void);
 
+//Sends a string to the Cellphone for custom commands
 void CELLSendCommand(char* String);
 
-void CELLToggleOnOff();        
+//Toggles the Cellphone modual on and off using the GPIO with a pulse
+void CELLToggleOnOff();    
 
+//This Gets a the cellphone to make a http reqest.
+//URL - is a string of the loction to make the request.
+//Param - is a array of strings for the params that will be added to a http get request
+//ParamSize - is an array of the chars in all the params in the array of strings (low level programming he)
+//Value - is a array of strings for the params values that will be added to a http get request
+//ValueSize - is an array of the chars in all the params values in the array of strings (low level programming hehe)
+//ParamsNum - The nunmber of paramerters
+//Response - if for your reponse buffer to fill
+//ResponseSize - The size of the response
 void CELLGetHTTP(char* URL, int URLsize, char** Params, int* ParamSize, char** Value, int* ValueSize, char* Response, int ResponseSize, byte ParamsNum);
